483cd8a5e600c49430c68d7394c37a8c79e9f626
Marty Whelan <martywhelan73@gmail.com>
2024-04-09 09:40:59 -0230

    setup the basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Timer from '$lib/components/Timer.svelte';
+	import { active } from '$lib/stores/timer';
+
+	let isBreak = false;
+	let workTime = 25;
+	let breakTime = 5;
+
+	function onComplete() {
+		isBreak = !isBreak;
+	}
+</script>
+
+<label for="work">Work</label>
+<input type="number" disabled={$active} name="work" bind:value={workTime} />
+<label for="rest">Rest</label>
+<input type="number" disabled={$active} name="rest" bind:value={breakTime} />
+
+<hr />
+
+<main>
+	{#if !isBreak}
+		<kbd>Working</kbd>
+		<Timer minutes={workTime} {onComplete} />
+	{:else}
+		<kbd>On Break</kbd>
+		<Timer minutes={breakTime} {onComplete} />
+	{/if}
+</main>
