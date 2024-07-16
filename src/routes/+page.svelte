791cf251377ee8359b26d77723ef02ce48953c5e
Marty Whelan <martywhelan73@gmail.com>
2024-07-16 09:48:29 -0230

    adding better notification handling via tauri

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,45 @@
 <script>
 	import Timer from '$lib/components/Timer.svelte';
 	import { active } from '$lib/stores/timer';
+	import { onMount } from 'svelte';
+	import { appWindow } from '@tauri-apps/api/window';
+	import {
+		isPermissionGranted,
+		sendNotification,
+		requestPermission
+	} from '@tauri-apps/api/notification';
 
+	let notificationsAllowed = false;
+	let permission = '';
 	let isBreak = false;
 	let workTime = 25;
 	let breakTime = 5;
+	let isWebview = false;
 
-	function onComplete() {
+	$: session = isBreak ? 'Work' : 'Break';
+
+	onMount(async () => {
+		notificationsAllowed = await isPermissionGranted();
+		if (!notificationsAllowed) {
+			permission = await requestPermission();
+			notificationsAllowed = permission === 'granted';
+		}
+		isWebview = '__TAURI_IPC__' in window;
+	});
+
+	async function onComplete() {
+		if (notificationsAllowed) {
+			sendNotification({
+				title: `${session} Complete!`,
+				body: `Your ${session} has finished. Hit start to begin the next one.`
+			});
+		}
+		if (isWebview) {
+			await appWindow.setFocus();
+		} else {
+			parent.focus();
+			window.focus();
+		}
 		isBreak = !isBreak;
 	}
 </script>
@@ -35,7 +68,9 @@
 		font-size: 22px;
 	}
 
-	:global(html), :global(body), .container {
+	:global(html),
+	:global(body),
+	.container {
 		height: 100%;
 	}
 
